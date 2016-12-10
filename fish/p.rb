82c984e01845f3a5168e536624d0ad97910b4b6a
jiangg <jiangg@mail.ustc.edu.cn>
2016-12-11 00:29:37 +0800

    alias p: Only grep process name

--- fish/p.rb
+++ fish/p.rb
@@ -3,7 +3,7 @@
 # e.g. ARGV is ssh, then arg is [s]sh.
 arg = "[#{ARGV.first[0]}]#{ARGV.first[1..-1]}"
 
-out = `ps aux | grep -v 'dot-files/fish/p.rb #{ARGV.first}' | grep -i --color=always #{arg}`
+out = `ps -e | grep -v 'dot-files/fish/p.rb #{ARGV.first}' | grep -i --color=always #{arg}`
 if out.empty?
   exit false
 else
